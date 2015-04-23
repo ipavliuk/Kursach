95f06e0f008bdc9d74654ffa5f3fc58f2aca3f6a
IgorP <igpavlyuk@gmail.com>
2015-04-23 09:39:39 +0300

    reservation view update

---
+++ Code/RentApartment.Web/RentAppartment.Client/ViewModels/ReservationsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using RentAppartment.Client.Utils;
+using RentAppartment.Client.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RentAppartment.Client.ViewModels
+{
+    public class ReservationsViewModel : ViewModelBase
+    {
+
+
+
+
+        //commands
+        private ICommand searchCommand;
+        public ICommand SearchCommand
+        {
+            get
+            {
+                if (this.searchCommand == null)
+                {
+                    this.searchCommand = new RelayCommand(o => this.SearchAction());
+                }
+                return this.searchCommand;
+            }
+        }
+
+        private void SearchAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
